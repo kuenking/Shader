a9e298c15d31451e2c91c36604c33f8c54c8567b
Shader <fzt@fzt-VirtualBox.(none)>
2015-05-12 22:24:15 +0800

    测试成功

--- game/works/source/main.c
+++ game/works/source/main.c
@@ -8,22 +8,23 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 ArgsPackage argsMsg;
-int    main(int  args,char **argv){
-     if   (args!=6){  
-       perror("args number error");
-       return  ARGS_NUM_ERROR;
-     }
-     RU(char filename[100];
-     sprintf(filename,"log_%s.txt",argv[5]););
-     conServer(argv);
-     LOG2F(filename,"链接完成");
-     char msg[MAXLENGTH];
-     while(1){
-       LOG2F(filename,"准备接受msg");
-       getMsg(msg);
-       if(-1==explMsg(msg))
-         break;
-     }
-     closeSocket();
-     return    0;
+int    main(int  args,char **argv) {
+    if   (args!=6) {
+        perror("args number error");
+        return  ARGS_NUM_ERROR;
+    }
+    RU(char filename[100];
+       sprintf(filename,"log_%s.txt",argv[5]););
+    conServer(argv);
+    LOG2F(filename,"链接完成");
+    char msg[MAXLENGTH];
+    while(1) {
+        LOG2F(filename,"准备接受msg");
+	memset(msg,0,MAXLENGTH);
+        getMsg(msg);
+        if(-1==explMsg(msg))
+            break;
+    }
+    closeSocket();
+    return    0;
 }
